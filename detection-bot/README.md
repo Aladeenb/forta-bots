4a4a37b76528364aba74e055102f63bc9f75b385
Ala Eddine Battikh <48839940+Aladeenb@users.noreply.github.com>
2023-01-24 02:34:02 +0200

    major fixes, removed unit testing

--- detection-bot/README.md
+++ detection-bot/README.md
@@ -13,7 +13,7 @@ This bot detects bots that are deployed by the Nethermind deployer address, [0x8
 Describe each of the type of alerts fired by this agent
 
 - NETHERMIND-1
-  - Fired when an occured transaction involves Nethermind's address
+  - Fired when an occured transaction involves Nethermind bot deployments address executes the contract that is responsible for updating and creating the bots
   - Severity is always set to "info"
   - Type is always set to "info"
   - Metadata field `address` included with this alert
@@ -22,4 +22,5 @@ Describe each of the type of alerts fired by this agent
 
 The bot behaviour can be verified with the following transactions:
 
-- 0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8 (Nethermind forta bot deployer address)
+- 0x5e42f98179ab8d554c22e2fe84f376378ce9d943269ef3395acd17d8b8a6c8c5 ([Update Bot Transaction Hash](https://polygonscan.com/tx/0x5e42f98179ab8d554c22e2fe84f376378ce9d943269ef3395acd17d8b8a6c8c5))
+- 0xca8355b309f8df12e720b0ac9f8bc38edfb3204748e9c29d769401daf2ee9d6c ([Create Bot Transaction Hash](https://polygonscan.com/tx/0xca8355b309f8df12e720b0ac9f8bc38edfb3204748e9c29d769401daf2ee9d6c))
--- detection-bot/src/agent.ts
+++ detection-bot/src/agent.ts
@@ -1,28 +1,25 @@
 import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingType } from "forta-agent";
 
-export const NETHERMIND_ADDRESS: { [address: string]: boolean } = {
-  "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8": true,
-};
+export const NETHERMIND_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
 
-let findingsCount = 0;
+export const DEPLOY_UPDATE_CONTRACT_ADDRESS = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
 
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
   const findings: Finding[] = [];
-  const nethermindAddress = Object.keys(txEvent.addresses).find((address) => NETHERMIND_ADDRESS[address]);
-  if (!nethermindAddress) return findings;
+  const deployUpdateTx = Object.keys(txEvent.addresses).find(() => DEPLOY_UPDATE_CONTRACT_ADDRESS);
+  if (!deployUpdateTx) return findings;
   findings.push(
     Finding.fromObject({
       name: "Nethermind Bot",
-      description: `Bot deployed/updated from Nethermind Forta deployer address (${nethermindAddress}).`,
-      alertId: "NETHERMIND-1", // Come up with better naming
+      description: `Bot deployed/updated from Nethermind Forta deployer address (${NETHERMIND_ADDRESS}).`,
+      alertId: "NETHERMIND-1",
       type: FindingType.Info,
       severity: FindingSeverity.Info,
       metadata: {
-        address: nethermindAddress,
+        string: txEvent.hash,
       },
     })
   );
-  findingsCount++;
   return findings;
 };
 
