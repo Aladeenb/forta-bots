e23cd53aac18d15f14917c60c16964689382eaba
Ala Eddine Battikh <48839940+Aladeenb@users.noreply.github.com>
2023-01-30 01:31:16 +0200

    modified bot's logic, changed mock tests

--- detection-bot/src/agent.spec.ts
+++ detection-bot/src/agent.spec.ts
@@ -1,53 +1,65 @@
-import { Finding, FindingSeverity, FindingType, HandleTransaction, createTransactionEvent } from "forta-agent";
-import agent from "./agent";
+import {
+  Finding,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+  createTransactionEvent,
+} from "forta-agent";
 
-export const NETHERMIND_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
+import agent, {
+  CREATE_BOT_FUNCTION,
+  UPDATE_BOT_FUNCTION,
+  NETHERMIND_ADDRESS,
+  DEPLOY_UPDATE_CONTRACT_ADDRESS,
+} from "./agent";
 
-export const DEPLOY_UPDATE_CONTRACT_ADDRESS = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
-
-describe("Nethermind bot deployment bot", () => {
+describe("Bot created", () => {
   let handleTransaction: HandleTransaction;
-
-  const createTxEventWithAddresses = (addresses: { [addr: string]: boolean }) =>
-    createTransactionEvent({
-      transaction: {} as any,
-      logs: [],
-      contractAddress: null,
-      block: {} as any,
-      addresses,
-    });
+  const mockTxEvent = createTransactionEvent({} as any);
 
   beforeAll(() => {
     handleTransaction = agent.handleTransaction;
   });
 
   describe("handleTransaction", () => {
-    it("returns empty findings if no bot is deployed/updated", async () => {
-      const txEvent = createTxEventWithAddresses({});
+    it("return empty findings if there are no bots created", async () => {
+      mockTxEvent.filterFunction = jest.fn().mockReturnValue([]);
 
-      const findings = await handleTransaction(txEvent);
+      const findings = await handleTransaction(mockTxEvent);
 
       expect(findings).toStrictEqual([]);
+      expect(mockTxEvent.filterFunction).toHaveBeenCalledTimes(2);
+      expect(mockTxEvent.filterFunction).toHaveBeenCalledWith(CREATE_BOT_FUNCTION, DEPLOY_UPDATE_CONTRACT_ADDRESS);
     });
 
-    it("returns a finding if a bot is deployed/updated", async () => {
-      const address = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
-      const txEvent = createTxEventWithAddresses({ [address]: true });
+    it("returns a finding if a bot is created", async () => {
+      const mockCreateBot = {
+        args: {
+          agentId: "0001",
+          owner: "0xabc",
+          metadata: "metadata",
+          chainIds: "[001, 002, 003, 004]",
+        },
+      };
+      mockTxEvent.filterFunction = jest.fn().mockReturnValue([mockCreateBot]);
 
-      const findings = await handleTransaction(txEvent);
+      const findings = await handleTransaction(mockTxEvent);
 
       expect(findings).toStrictEqual([
         Finding.fromObject({
           name: "Nethermind Bot",
-          description: `Bot deployed/updated from Nethermind Forta deployer address (${NETHERMIND_ADDRESS}).`,
+          description: `Bot created from (${NETHERMIND_ADDRESS}).`,
           alertId: "NETHERMIND-1",
           type: FindingType.Info,
           severity: FindingSeverity.Info,
           metadata: {
-            string: address,
+            from: mockCreateBot.args.owner,
           },
         }),
       ]);
+      expect(mockTxEvent.filterFunction).toHaveBeenCalledTimes(2);
+      expect(mockTxEvent.filterFunction).toHaveBeenCalledWith(CREATE_BOT_FUNCTION, DEPLOY_UPDATE_CONTRACT_ADDRESS);
     });
   });
 });
--- detection-bot/src/agent.ts
+++ detection-bot/src/agent.ts
@@ -1,25 +1,66 @@
 import { Finding, HandleTransaction, TransactionEvent, FindingSeverity, FindingType } from "forta-agent";
 
+export const CREATE_BOT_FUNCTION =
+  "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
+export const UPDATE_BOT_FUNCTION = "function updateAgent(uint256 agentId, string metadata, uint256[] chainIds)";
 export const NETHERMIND_ADDRESS = "0x88dC3a2284FA62e0027d6D6B1fCfDd2141a143b8";
-
 export const DEPLOY_UPDATE_CONTRACT_ADDRESS = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
+// export const DEPLOY_UPDATE_CONTRACT_ADDRESS_2 = "0xB50d3960a49120D0A6B543E7295cAE6C78d07967";
 
 const handleTransaction: HandleTransaction = async (txEvent: TransactionEvent) => {
   const findings: Finding[] = [];
-  const deployUpdateTx = Object.keys(txEvent.addresses).find(() => DEPLOY_UPDATE_CONTRACT_ADDRESS);
-  if (!deployUpdateTx) return findings;
-  findings.push(
-    Finding.fromObject({
-      name: "Nethermind Bot",
-      description: `Bot deployed/updated from Nethermind Forta deployer address (${NETHERMIND_ADDRESS}).`,
-      alertId: "NETHERMIND-1",
-      type: FindingType.Info,
-      severity: FindingSeverity.Info,
-      metadata: {
-        string: deployUpdateTx,
-      },
-    })
-  );
+
+  // filter functions for create or update bots
+  const createBots = txEvent.filterFunction(CREATE_BOT_FUNCTION, DEPLOY_UPDATE_CONTRACT_ADDRESS);
+  const updateBots = txEvent.filterFunction(UPDATE_BOT_FUNCTION, DEPLOY_UPDATE_CONTRACT_ADDRESS);
+
+  const createUpdateTransaction = Object.keys(txEvent.addresses).find(() => NETHERMIND_ADDRESS);
+
+  Array.from(createBots).forEach((createBot) => {
+    // extract bot creation arguments
+    const { owner } = createBot.args;
+
+    if (!createBots && !updateBots && !createUpdateTransaction) return findings;
+    // A bot was created
+    else if (createUpdateTransaction && createBots && !updateBots) {
+      //TODO recheck if it is needed
+      findings.push(
+        Finding.fromObject({
+          name: "Nethermind Bot",
+          description: `Bot created from (${NETHERMIND_ADDRESS}).`,
+          alertId: "NETHERMIND-1",
+          type: FindingType.Info,
+          severity: FindingSeverity.Info,
+          metadata: {
+            from: owner,
+          },
+        })
+      );
+    }
+  });
+
+  Array.from(updateBots).forEach((updateBot) => {
+    // extract the needed arguments
+    const { owner } = updateBot.args;
+
+    if (createUpdateTransaction && !createBots && !updateBots) return findings;
+    // A bot was updated
+    else if (!createBots && updateBots) {
+      findings.push(
+        Finding.fromObject({
+          name: "Nethermind Bot",
+          description: `Bot updated from (${NETHERMIND_ADDRESS}).`,
+          alertId: "NETHERMIND-2",
+          type: FindingType.Info,
+          severity: FindingSeverity.Info,
+          metadata: {
+            from: owner,
+          },
+        })
+      );
+    }
+  });
+
   return findings;
 };
 
